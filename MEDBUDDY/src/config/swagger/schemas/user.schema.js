845d59eca1d0b1b6895f5c166b142b038b7ad179
duchuy188 <huyndse173390@fpt.edu.vn>
2025-08-08 10:24:34 +0700

    Update user.schema.js

--- MEDBUDDY/src/config/swagger/schemas/user.schema.js
+++ MEDBUDDY/src/config/swagger/schemas/user.schema.js
@@ -1,1 +1,20 @@
 // Swagger schema for user
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         role:
+ *           type: string
+ *           enum: [user, admin]
+ */
+module.exports = {};
