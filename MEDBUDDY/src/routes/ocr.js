8fb53fefc05b917b69d475029e23b1f4e356ef1e
Phan Văn Trường <phantryong@gmail.com>
2025-10-03 23:18:10 +0700

    feat(ocr-service): add middleware for ocr upload image

--- MEDBUDDY/src/routes/ocr.js
+++ MEDBUDDY/src/routes/ocr.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const upload = require('../middlewares/upload.middleware');
-const auth = require('../middlewares/auth.middleware');
-const ocrController = require('../controllers/ocr.controller');
+const upload = require("../middlewares/upload.middleware");
+const auth = require("../middlewares/auth.middleware");
+const ocrController = require("../controllers/ocr.controller");
+const { isPaidUser } = require("../middlewares/ocr.middleware");
 
 // POST /api/ocr (có phân quyền)
-router.post('/', auth, upload.single('image'), ocrController.ocrPrescription);
+router.post(
+  "/",
+  auth,
+  isPaidUser,
+  upload.single("image"),
+  ocrController.ocrPrescription
+);
 
 // GET /api/ocr/test-cloudinary (test connection)
-router.get('/test-cloudinary', ocrController.testCloudinary);
+router.get("/test-cloudinary", ocrController.testCloudinary);
 
 module.exports = router;
