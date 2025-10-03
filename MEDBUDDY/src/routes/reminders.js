91b817b740639f9ca1be9b678e34c1fafbb7e31b
Phan Văn Trường <phantryong@gmail.com>
2025-10-03 23:25:46 +0700

    feat(reminder-service): add middleware is user paid for package to use reminder

--- MEDBUDDY/src/routes/reminders.js
+++ MEDBUDDY/src/routes/reminders.js
@@ -1,22 +1,33 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const authMiddleware = require('../middlewares/auth.middleware');
-const remindersController = require('../controllers/reminders.controller');
+const authMiddleware = require("../middlewares/auth.middleware");
+const remindersController = require("../controllers/reminders.controller");
+const { isPaidUser } = require("../middlewares/ocr.middleware");
 
 // GET /reminders – Danh sách nhắc uống thuốc
-router.get('/', authMiddleware, remindersController.getReminders);
+router.get("/", authMiddleware, remindersController.getReminders);
 
 // POST /reminders – Thêm nhắc uống thuốc
-router.post('/', authMiddleware, remindersController.createReminder);
+router.post(
+  "/",
+  authMiddleware,
+  isPaidUser,
+  remindersController.createReminder
+);
 
 // GET /reminders/:id – Xem chi tiết nhắc nhở
-router.get('/:id', authMiddleware, remindersController.getReminderById);
+router.get("/:id", authMiddleware, remindersController.getReminderById);
 
 // PUT /reminders/:id – Cập nhật nhắc nhở
-router.put('/:id', authMiddleware, remindersController.updateReminder);
+router.put(
+  "/:id",
+  authMiddleware,
+  isPaidUser,
+  remindersController.updateReminder
+);
 
 // DELETE /reminders/:id – Xóa nhắc nhở
-router.delete('/:id', authMiddleware, remindersController.deleteReminder);
-    
+router.delete("/:id", authMiddleware, remindersController.deleteReminder);
+
 module.exports = router;
